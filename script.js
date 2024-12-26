f5c5ccc1d4a2b45a66b13ca2bbec8924c08f7f6a
Aafreen2 <aafreen.hf11@gmail.com>
2024-12-27 04:24:12 +0530

    adding all files

---
+++ script.js
@@ -0,0 +1,10 @@
+// get a reference to the div element using its ID
+var divElement = document.getElementById("myDiv");
+divElement.style.color = "black";
+divElement.style.backgroundColor = "lightblue";
+divElement.style.margin = "40px";
+divElement.style.padding = "20px";
+divElement.style.fontSize = "28px";
+divElement.style.fontWeight = "thin";
+divElement.style.height = "400px";
+divElement.style.width = "600px";
